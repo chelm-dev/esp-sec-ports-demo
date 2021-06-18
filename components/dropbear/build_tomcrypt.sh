a613826e7886bb7883b4945c559d46519fc184ab
Chris Chelmecki <chris.chelmecki@gmail.com>
2021-06-18 13:46:11 -0500

    Added makefiles
    Files are normally autogen'd during configure

--- components/dropbear/build_tomcrypt.sh
+++ components/dropbear/build_tomcrypt.sh
@@ -1,3 +1,6 @@
 CFLAGS="-I../ -I../../ -DLOCALOPTIONS_H_EXISTS -DLTC_EASY -mlongcalls"
+cp Makefile.dropbear dropbear/Makefile
+cp Makefile.libtomcrypt dropbear/libtomcrypt/Makefile
+cp Makefile.libtommath dropbear/libtommath/Makefile
 make -C dropbear/libtomcrypt CC=xtensa-esp32-elf-gcc RANLIB=xtensa-esp32-elf-ranlib AR=xtensa-esp32-elf-ar CFLAGS="$CFLAGS" clean libtomcrypt.a
 make -C dropbear/libtommath CC=xtensa-esp32-elf-gcc RANLIB=xtensa-esp32-elf-ranlib AR=xtensa-esp32-elf-ar CFLAGS="$CFLAGS"  clean libtommath.a
